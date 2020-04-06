a78c8a36f52c4dfd46c9363ff8dd709996a4ad41
Artem Kovalov <artem.kovalyov@gmail.com>
2020-04-06 17:12:53 +0200

    Deploy website version based on 163b2a076f5cd708e8f9adc4661c6205d5886917

---
+++ 7dd4a2d1.d1d180e7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{129:function(o){o.exports=JSON.parse('{"allTagsPath":"/ph-stone/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/ph-stone/blog/tags/hello"}')}}]);
