8efbc7797c66a5a709056b9f077c2ad151e9f42b
Artem Kovalov <artem.kovalyov@gmail.com>
2020-04-14 03:24:52 +0200

    Deploy website version based on

---
+++ 1c7bd35a.cc420ec1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{145:function(s){s.exports=JSON.parse('{"allTagsPath":"/ph-stone/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/ph-stone/blog/tags/docusaurus"}')}}]);
